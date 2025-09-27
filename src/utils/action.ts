8f55c067245f6c8d93825e2fe804b6bff30e75d9
key-68 <tkhoa06082005@gmailcom>
2025-09-28 00:00:38 +0700

    jwt login

--- src/utils/action.ts
+++ src/utils/action.ts
@@ -1,16 +1,34 @@
 'use server'
 import { signIn } from "@/auth";
 
-export async function authenticate(email: string, password: string) {
+export async function authenticate(username: string, password: string) {
   try {
     const r = await signIn("credentials", {
-      username: email,
+      username: username,
       password: password,
       redirect: false,
     })
     return r
   } catch (error) {
-    return { "error": "Incorrect username or password" }
+    if((error as any).name === 'InvalidEmailPasswordError') {
+      return {
+        error: (error as any).type,
+        code: 1,
+      }
+    }
+    else if ((error as any).name === 'InactiveAccountError') {
+      return {
+        error: (error as any).type,
+        code: 2,
+      }
+    }
+    else {
+      return {
+        error: 'Internal server error',
+        code: 0,
+      }
+    }
+
     // if (error.cause.err instanceof InvalidLoginError) {
     //   return { "error": "Incorrect username or password" }
     // } else {
